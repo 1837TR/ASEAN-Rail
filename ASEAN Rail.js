506b77e089aced4345af1303f1c622b01f7cc688
1837TR <Kamroncodes@pm.me>
2024-10-21 20:27:05 +0700

    "v0.4"

--- ASEAN Rail.js
+++ ASEAN Rail.js
@@ -125,37 +125,34 @@ hoverMenuPicKl.onclick = klLink;
 
 //Map
 
-let kunmingTextBox = document.getElementsByClassName('textbox')[0];
 let kunmingText = document.getElementsByClassName('text')[0];
 let kunmingMapPoint = document.getElementsByClassName('mappoint')[0];
 
 function kunmingCityNamePopUp() {
-    kunmingTextBox.style.display = "inline";
     kunmingText.style.display = "inline";
-    kunmingMapPoint.style.fill = "#F6EC5F";
+    kunmingMapPoint.style.fill = "#DD4F40";
 }
 
 function kunmingCityNameRevert() {
-    kunmingTextBox.style.display = "none";
     kunmingText.style.display = "none";
-    kunmingMapPoint.style.fill = "#DD4F40";
+    kunmingMapPoint.style.fill = "#487ab9";
 }
 
 kunmingMapPoint.onmouseover = kunmingCityNamePopUp;
 kunmingMapPoint.onmouseout = kunmingCityNameRevert;
 
-let vientianeTextBox = document.getElementsByClassName('textbox')[1];
+let vientianeTextBox = document.getElementsByClassName('textbox')[0];
 let vientianeMapPoint = document.getElementsByClassName('mappoint')[1];
-let vientianeText = document.getElementsByClassName('text')[1];
+let vientianeText = document.getElementsByClassName('text')[8];
 
 function vientianeCityNamePopUp() {
-    vientianeMapPoint.style.fill = "#F6EC5F";
+    vientianeMapPoint.style.fill = "#DD4F40";
     vientianeTextBox.style.display = "inline";
     vientianeText.style.display = "inline";
 }
 
 function vientianeCityNameRevert(){
-    vientianeMapPoint.style.fill = "#DD4F40";
+    vientianeMapPoint.style.fill = "#487ab9";
     vientianeTextBox.style.display = "none";
     vientianeText.style.display = "none";
 }
@@ -163,132 +160,114 @@ function vientianeCityNameRevert(){
 vientianeMapPoint.onmouseover = vientianeCityNamePopUp;
 vientianeMapPoint.onmouseout = vientianeCityNameRevert;
 
-let khonkaenTextBox = document.getElementsByClassName('textbox')[2];
-let khonkaenText = document.getElementsByClassName('text')[2];
-let khonkaenMapPoint = document.getElementsByClassName('mappoint')[8];
+let khonkaenTextBox = document.getElementsByClassName('textbox')[1];
+let khonkaenText2 = document.getElementsByClassName('text')[9];
+let khonkaenText = document.getElementsByClassName('text')[10];
+let khonkaenMapPoint = document.getElementsByClassName('mappoint')[2];
 
 khonkaenMapPoint.onmouseover = function() {
-    khonkaenMapPoint.style.fill = '#F6EC5F';
+    khonkaenMapPoint.style.fill = '#DD4F40';
     khonkaenText.style.display = "inline";
     khonkaenTextBox.style.display = "inline";
+    khonkaenText2.style.display = "inline";
 };
 
 khonkaenMapPoint.onmouseout = function() {
-    khonkaenMapPoint.style.fill = '#DD4F40';
+    khonkaenMapPoint.style.fill = '#487ab9';
     khonkaenText.style.display = "none";
+    khonkaenText2.style.display = "none";
     khonkaenTextBox.style.display = "none";
 };
 
-let phuketTextBox = document.getElementsByClassName('textbox')[5];
-let phuketText = document.getElementsByClassName('text')[5];
-let phuketMapPoint = document.getElementsByClassName('mappoint')[3];
+let phuketText = document.getElementsByClassName('text')[1];
+let phuketMapPoint = document.getElementsByClassName('mappoint')[5];
 
 phuketMapPoint.onmouseover = function() {
-    phuketMapPoint.style.fill = "#F6EC5F";
+    phuketMapPoint.style.fill = "#DD4F40";
     phuketText.style.display = "inline";
-    phuketTextBox.style.display = "inline";
 }
 
 phuketMapPoint.onmouseout = function() {
-    phuketMapPoint.style.fill = "#DD4F40";
-    phuketTextBox.style.display = "none";
+    phuketMapPoint.style.fill = "#487ab9";
     phuketText.style.display = "none";
 }
 
-let bkkMapPoint = document.getElementsByClassName('mappoint')[2];
-let bkkText = document.getElementsByClassName('text')[3];
-let bkkTextBox = document.getElementsByClassName('textbox')[3];
+let bkkMapPoint = document.getElementsByClassName('mappoint')[3];
+let bkkText = document.getElementsByClassName('text')[7];
 
 bkkMapPoint.onmouseover = function() {
-   bkkMapPoint.style.fill = "#F6EC5F";
+   bkkMapPoint.style.fill = "#DD4F40";
    bkkText.style.display = "inline";
-   bkkTextBox.style.display = "inline";
 }
 
 bkkMapPoint.onmouseout = function() {
-    bkkMapPoint.style.fill = "#DD4F40";
-    bkkTextBox.style.display = "none";
+    bkkMapPoint.style.fill = "#487ab9";
     bkkText.style.display = "none";
 }
 
 
-let huahinMapPoint = document.getElementsByClassName('mappoint')[7];
-let huahinText = document.getElementsByClassName('text')[4];
-let huahinTextBox = document.getElementsByClassName('textbox')[4];
+let huahinMapPoint = document.getElementsByClassName('mappoint')[4];
+let huahinText = document.getElementsByClassName('text')[2];
 
 huahinMapPoint.onmouseover = function() {
-   huahinMapPoint.style.fill = "#F6EC5F";
+   huahinMapPoint.style.fill = "#DD4F40";
    huahinText.style.display = "inline";
-   huahinTextBox.style.display = "inline";
 }
 
 huahinMapPoint.onmouseout = function() {
-    huahinMapPoint.style.fill = "#DD4F40";
-    huahinTextBox.style.display = "none";
+    huahinMapPoint.style.fill = "#487ab9";
     huahinText.style.display = "none";
 }
 
-let johorMapPoint = document.getElementsByClassName('mappoint')[4];
-let johorText = document.getElementsByClassName('text')[7];
-let johorTextBox = document.getElementsByClassName('textbox')[7];
+let johorMapPoint = document.getElementsByClassName('mappoint')[8];
+let johorText = document.getElementsByClassName('text')[5];
 
 johorMapPoint.onmouseover = function() {
-   johorMapPoint.style.fill = "#F6EC5F";
+   johorMapPoint.style.fill = "#DD4F40";
    johorText.style.display = "inline";
-   johorTextBox.style.display = "inline";
 }
 
 johorMapPoint.onmouseout = function() {
-    johorMapPoint.style.fill = "#DD4F40";
-    johorTextBox.style.display = "none";
+    johorMapPoint.style.fill = "#487ab9";
     johorText.style.display = "none";
 }
 
-let singaporeMapPoint = document.getElementsByClassName('mappoint')[5];
-let singaporeText = document.getElementsByClassName('text')[8];
-let singaporeTextBox = document.getElementsByClassName('textbox')[8];
+let singaporeMapPoint = document.getElementsByClassName('mappoint')[9];
+let singaporeText = document.getElementsByClassName('text')[6];
 
 singaporeMapPoint.onmouseover = function() {
-   singaporeMapPoint.style.fill = "#F6EC5F";
+   singaporeMapPoint.style.fill = "#DD4F40";
    singaporeText.style.display = "inline";
-   singaporeTextBox.style.display = "inline";
 }
 
 singaporeMapPoint.onmouseout = function() {
-    singaporeMapPoint.style.fill = "#DD4F40";
-    singaporeTextBox.style.display = "none";
+    singaporeMapPoint.style.fill = "#487ab9";
     singaporeText.style.display = "none";
 }
 
 let penangMapPoint = document.getElementsByClassName('mappoint')[6];
-let penangText = document.getElementsByClassName('text')[9];
-let penangTextBox = document.getElementsByClassName('textbox')[9];
+let penangText = document.getElementsByClassName('text')[3];
 
 penangMapPoint.onmouseover = function() {
-   penangMapPoint.style.fill = "#F6EC5F";
+   penangMapPoint.style.fill = "#DD4F40";
    penangText.style.display = "inline";
-   penangTextBox.style.display = "inline";
 }
 
 penangMapPoint.onmouseout = function() {
-    penangMapPoint.style.fill = "#DD4F40";
-    penangTextBox.style.display = "none";
+    penangMapPoint.style.fill = "#487ab9";
     penangText.style.display = "none";
 }
 
-let kualalumpurMapPoint = document.getElementsByClassName('mappoint')[9];
-let kualalumpurText = document.getElementsByClassName('text')[6];
-let kualalumpurTextBox = document.getElementsByClassName('textbox')[6];
+let kualalumpurMapPoint = document.getElementsByClassName('mappoint')[7];
+let kualalumpurText = document.getElementsByClassName('text')[4];
 
 kualalumpurMapPoint.onmouseover = function() {
-   kualalumpurMapPoint.style.fill = "#F6EC5F";
+   kualalumpurMapPoint.style.fill = "#DD4F40";
    kualalumpurText.style.display = "inline";
-   kualalumpurTextBox.style.display = "inline";
 }
 
 kualalumpurMapPoint.onmouseout = function() {
-    kualalumpurMapPoint.style.fill = "#DD4F40";
-    kualalumpurTextBox.style.display = "none";
+    kualalumpurMapPoint.style.fill = "#487ab9";
     kualalumpurText.style.display = "none";
 }
 
